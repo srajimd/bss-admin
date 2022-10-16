7392bab25b175fe1234c1fed6255e953199ea1b8
srajimd <rajeswarimca@gmail.com>
2022-10-16 09:09:39 +0530

    updates

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -62,6 +62,7 @@ class Enrollment extends Model
 
     public function scopeActive($query)
     {
-        return $query->where('status', 1)->whereDate('expiry_date','<',now());
+        $now = Carbon::now();
+        return $query->where('status', 1)->whereDate('expiry_date','<',$now);
     }
 }
